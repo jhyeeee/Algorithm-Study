e879b845d2091313349a4c60bfcb76449cb0fdcd
jhyeeee <104542485+jhyeeee@users.noreply.github.com>
2023-07-31 10:55:51 +0900

    2023-07-31 / boj-1676
    boj-1676

---
+++ src/boj_1676/Main.java
@@ -0,0 +1,30 @@
+package boj_1676;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		BigInteger result = new BigInteger("1");
+		for(int i = 1; i <= n; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		
+		String str = String.valueOf(result);
+		int cnt = 0;
+		for(int i = str.length()-1; i >= 0; i--) {
+			if(str.charAt(i)-'0' == 0) {
+				cnt++;
+			}else {
+				break;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+}
